a8b79ab753480ef9a91b8d78614d179ebfb0348b
byq818 <yiqi.bai@gmail.com>
2015-01-03 21:39:57 -0500

    2 Version

--- src/com/byq/leetcode/BinaryTreeLevelOrderTraversalII/solution.java
+++ src/com/byq/leetcode/BinaryTreeLevelOrderTraversalII/solution.java
@@ -10,39 +10,34 @@ import java.util.Queue;
  * Created by yiqibai on 1/3/15.
  */
 public class solution {
+
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         if(root == null)
             return new ArrayList<List<Integer>>();
         else{
             Queue<TreeNode> tQueue = new ArrayDeque<TreeNode>();
-            List<List<TreeNode>> tlist = new ArrayList<List<TreeNode>>();
             tQueue.add(root);
             List<List<Integer>> rt = new ArrayList<List<Integer>>();
 
             while (true){
-                List<TreeNode> lv = new ArrayList<TreeNode>();
-                while(tQueue.size() > 0)
-                    lv.add(tQueue.poll());
-                if(lv.size() > 0){
-                    tlist.add(lv);
-                    for(TreeNode n : lv){
-                        if(n.left != null)
-                            tQueue.add(n.left);
-                        if(n.right != null)
-                            tQueue.add(n.right);
-                    }
-                }else
-                    break;
-            }
+                List<Integer> val = new ArrayList<Integer>();
+                Queue<TreeNode> level = new ArrayDeque<TreeNode>();
 
-            for(int i = tlist.size() -1; i >= 0 ; i --){
-                List<TreeNode> tl = tlist.get(i);
-                List<Integer> lev = new ArrayList<Integer>();
-                for(TreeNode n : tl)
-                    lev.add(n.val);
-                rt.add(lev);
+                while(tQueue.size() > 0){
+                    TreeNode node = tQueue.poll();
+                    val.add(node.val);
+                    if(node.left != null)
+                        level.add(node.left);
+                    if(node.right != null)
+                        level.add(node.right);
+                }
+                if(level.size() > 0){
+                    tQueue = level;
+                    rt.add(0, val);
+                }
+                else break;
             }
-            return rt;
+        return rt;
         }
     }
 }
